10c30c0fb56148c9c5dbb08fd30922c3257244f5
Cleiton Cardoso <cleiton_kssa@hotmail.com>
2022-09-13 19:02:21 -0300

    Cleiton Cardoso 13/09/2022 19h00 Header Finalizado HTML / CSS

--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -1,12 +1,27 @@
-import  '../styles/style_headermobile.css'
-import Logo from '../images/logo.png'
-import Search from '../images/search.png'
-import Carrinho from '../images/carrinho.png'
+import '../styles/style_headermobile.css'
+import Logo from '../images/logo.svg'
+import Search from '../images/search.svg'
+import Carrinho from '../images/carrinho.svg'
 import User from '../images/iconuser.svg'
-// import React, { useEffect, useState } from 'react'
+import Monitor from '../images/monitor.svg'
+import Processador from '../images/processador.svg'
+import Placavideo from '../images/placavideo.svg'
+import Memoriaram from '../images/memoriaram.svg'
+import Hd from '../images/hd.svg'
+import Menu from '../images/menu.svg'
+import Fechar from '../images/fechar.svg'
+import React, { useEffect, useState } from 'react'
 
 
-const HeaderMobile = () =>{
+
+const HeaderMobile = () => {
+
+  const [menu, setMenu] = useState(false)
+
+  function handleClick() {
+    setMenu(!menu)
+    console.log(menu)
+  }
 
   // const [show,setShow] = useState(false);
   // const controlNavbar = () =>{
@@ -25,60 +40,76 @@ const HeaderMobile = () =>{
   // },[])
 
   return (
-  <>
-      <div className='box-headerm'>
-      <div className='area-logo'>
-        <div className='txt-logo'>
-          <div className='div-logo'>
-         <img src={Logo}/>
-         </div>
-        <div>
-        <p>CONSOLE</p>
-        </div>
-       
-        </div>
-
-        <div className='area-input'>
-          <input className='input-search' id="search-input" type="text" />
-          <div className='lupa-search'>
-            <img src={Search}/>
-          </div>
-        </div>
-
-   
-
-        <div className='txt-area-login-icon'>
-
-        <div className='carrinho-compras'>
-        <img src={Carrinho}/>
-        </div>
-        <div className='icon-user'>
-        <img src={User}/>
-        </div>
-
-        <div className='txt-area-login'>
-           <p>Faça seu <strong>Login</strong> ou <br/> crie seu <strong>Cadastro</strong></p></div>
-        </div>
-
+    <>
+      <div className={menu ? 'mascarashow' : 'mascarahide'} onClick={handleClick}>
       </div>
 
-    </div>
+      <div className='box-headerm'>
 
-   <div className='header-total-showm'>
-    <div className='nav-total'>
-      <nav>
-        <ul>
-          <li> <a>Monitor</a> <span className='monitor'></span></li>
-          <li> <a>Processador</a></li>
-          <li> <a>Placa de vídeo</a></li>
-          <li> <a>Memória RAM</a></li>
-          <li> <a>Armazenamento</a></li>
-        </ul>
-      </nav>
-    </div>
+        <div className='area-logom'>
 
-   </div>
-  </>
+          <div className='div-menu'>
+            <img src={Menu} onClick={handleClick} />
+          </div>
+
+
+
+          <div className='area-input'>
+            <input className='input-search' id="search-input" type="text" />
+
+            <div className='lupa-search'>
+              <img src={Search} />
+            </div>
+          </div>
+
+          <div className='txt-logom'>
+            <img src={Logo} />
+            <p>CONSOLE</p>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div className='header-total-showm'>
+        <div className={menu ? 'nav-total' : 'nav-totalhiden'}>
+          <nav>
+            <img src={Fechar} className='btn-fechar' onClick={handleClick} />
+            <div className='icons-nav'>
+              <div className='div-usuario'>
+              <img src={User}  className='btn-user'/><a>Login</a>
+              </div>
+              <div className='div-carrinho'>
+              <img src={Carrinho} className='btn-carrinho' /><a>Minhas Compras</a>
+              </div>
+            </div>
+            <ul>
+
+              <li>
+                <img src={Monitor} />
+                <a>Monitor</a>
+              </li>
+              <li>
+                <img src={Processador} />
+                <a>Processador</a>
+              </li>
+              <li>
+                <img src={Placavideo} />
+                <a>Placa de vídeo</a>
+              </li>
+              <li>
+                <img src={Memoriaram} />
+                <a>Memória RAM</a>
+              </li>
+              <li>
+                <img src={Hd} />
+                <a>Armazenamento</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
   )
 
 }
