5373570dae96fba2374b6418890de80497f522cc
Cleiton Cardoso <cleiton_kssa@hotmail.com>
2022-09-15 20:21:11 -0300

    Cleiton Cardoso 15/09/2022

--- src/components/BannerOne.jsx
+++ src/components/BannerOne.jsx
@@ -10,8 +10,10 @@ export default function BannerOne() {
           <img src={Tv} />
         </div>
         <div className='div-txt-btn-banner'>
+          <div>
           <p className='titulo-banner'>Monitor gamer asus republic of games 24.5'ips</p>
           <p className='descricao-produto'>Com um suporte ergonomicamente projetado, ele fornece ajustes de inclinação, rotação, eixo e altura para que você possa encontrar facilmente sua posição de visualização ideal. O display também é compatível com VESA para montagem na parede.</p>
+          </div>
           <div className= 'div-preco-e-btn'>
           <p className='preco'>R$5.099,99</p>
           <button className='btn-comprar'>Comprar</button>
