74038a3c743c0f99c8acaae80bd276dab387886a
Hyago Vieira <hyago@okn.com.br>
2023-01-28 14:19:45 -0300

    Ajuste na home para exibir de forma dinamica as categorias

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,21 +4,34 @@ import SliderCards from "../../components/Cards/SliderCards";
 import { Card } from '../../components/Cards/Card';
 import { useEffect } from 'react';
 import { listProductByCategory } from "../../api/enpoints/products/list-product-by-category";
+import { listAllCategory } from "../../api/enpoints/categories/list-all-category";
 import Loading from '../../components/Ui/Loading/Loading';
 import { useState } from "react";
 
 const Home = () => {
 
   const [loading, setLoading] = useState(true);
-  const [monitors, setMonitors] = useState([]);
-  const [processors, setProcessors] = useState([]);
+  const [products, setProducts] = useState({});
 
   useEffect(() => {
     (async () => {
-      const monitors = await listProductByCategory("monitor", 8);
-      const processors = await listProductByCategory("processador", 8);
-      setMonitors(monitors.rows);
-      setProcessors(processors.rows);
+      const allCategories = await listAllCategory();
+      
+      let productsByCategory = []
+
+      for(let category of allCategories){
+        const categoryProducts = await listProductByCategory(category.slug, 8);
+
+        if(categoryProducts.rows.length > 1){
+          productsByCategory.push({
+            name: category.name,
+            plural_name: category.plural_name,
+            slug: category.slug,
+            rows: categoryProducts.rows
+          })
+        }
+      }
+      setProducts(productsByCategory);
       setLoading(false);
     })();
   }, []) // eslint-disable-line
@@ -31,35 +44,26 @@ const Home = () => {
     <>
     <Sliderhome /> 
     <main>
-      <SliderCards category="Monitor" link="monitor">
-        {monitors.map((monitor) => {
+      {
+        products.map((productByCategory, index) => {
           return (
-            <Card
-              image={monitor.image_products[0].name}
-              name={monitor.name}
-              description={monitor.description}
-              price={monitor.price}
-              id={monitor.id}
-              key={monitor.id}
-            />
+            <SliderCards category={productByCategory.plural_name} link={productByCategory.slug} key={index}>
+              {productByCategory.rows.map((productItem) => {
+                return (
+                  <Card 
+                    image={productItem.image_products[0].name}
+                    name={productItem.name}
+                    description={productItem.description}
+                    price={productItem.price}
+                    id={productItem.id}
+                    key={productItem.id}
+                  />
+                )
+              })}
+            </SliderCards>
           )
-        })}
-      </SliderCards>
-
-      <SliderCards category="Processador" link="processador">
-        {processors.map((processor) => {
-          return (
-            <Card
-              image={processor.image_products[0].name}
-              name={processor.name}
-              description={processor.description}
-              price={processor.price}
-              id={processor.id}
-              key={processor.id}
-            />
-          )
-        })}
-      </SliderCards>
+        })
+      }
     </main>
     </>
   )
