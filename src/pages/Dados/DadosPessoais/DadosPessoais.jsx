228f11015a034f45b0f1a455ccabb5b1fe83edd0
Aline Alegre <alinealegre@icloud.com>
2023-02-11 18:16:45 -0300

    Finalização página dados cadastrais.

--- src/pages/Dados/DadosPessoais/DadosPessoais.jsx
+++ src/pages/Dados/DadosPessoais/DadosPessoais.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { FaUserAlt }  from "react-icons/fa"
 import MenuPerfil from "../../../components/Dados/MenuPerfil/MenuPerfil";
 import "./style.dadosPessoais.css"; 
+import ButtonPrimary from "../../../components/Ui/ButtonPrimary/ButtonPrimary";
 import Input from "../../../components/Ui/Input/Input";
 
 const DadosPessoais = () => {
@@ -13,11 +15,30 @@ const DadosPessoais = () => {
             </div>
 
             <div className="page-dados-pessais-form">
-                <h1>FORM</h1>
-                <label htmlFor="nome">Nome:</label>
-                <Input value="Nome" id="nome"/>
-            </div>
+                <div>
+                    <div className="login-title">
+                    <h1><FaUserAlt className="login-title-icon"/> Dados Pessoais</h1>
+                    </div>  
+                    <form className="form-dados-pessoais"> 
+                        <label htmlFor="nome">Nome:</label>                        
+                        <Input value="Nome" />
 
+                        <label htmlFor="email">Email: </label>                      
+                        <Input value="nome@gmail.com" id="email" />     
+
+                        <label htmlFor="cpf">CPF:</label>
+                        <Input value="xxx.xxx.xxx-xxx" />                       
+                    
+                        <label>Data de Nascimento:</label>
+                        <Input type="date" value="birthdate" required />
+
+                        <ButtonPrimary>Atualizar</ButtonPrimary>           
+                    </form>           
+
+                </div>
+                           
+            </div>
+            
         </main>
     );
 
